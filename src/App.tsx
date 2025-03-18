a474a1796a9429decbc8bf309f637188a99de06d
sira3407 <140860448+sira3407@users.noreply.github.com>
2025-03-18 17:03:47 +0000

    Add React and React Router dependencies, along with initial type definitions and server components

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import StudentProgress from './components/StudentProgress';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/students/:id" element={<StudentProgress />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
